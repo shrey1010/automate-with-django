efd8cc0a0d941596b2c353fd3e0770e546878548
Shrey <shreyshukla1010@gmail.com>
2024-07-19 01:18:24 +0530

    autocomplete dropdown

--- stockanalysis/urls.py
+++ stockanalysis/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("stocks",views.stocks , name="stocks"),
+    path("stocks/",views.stocks , name="stocks"),
+    
+    path("stock-autocomplete/",views.StockAutocomplete.as_view(),name="stock_autocomplete"),
 ]
--- stockanalysis/views.py
+++ stockanalysis/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
+from dal import autocomplete
+from .models import Stock
+from .forms import StockForm
 # Create your views here.
 
 def stocks(request):
-    return render(request, "stockanalysis/stocks.html")
+    form = StockForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "stockanalysis/stocks.html",context=context)
+
+class StockAutocomplete(autocomplete.Select2QuerySetView):
+     def get_queryset(self):
+        if not self.request.user.is_authenticated:
+             return Stock.objects.none()
+        
+        qs = Stock.objects.all()
+        if self.q:
+            qs = qs.filter(name__istartswith=self.q)
+            
+        return qs
